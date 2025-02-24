30c415d826aa8b0c0d12ab462f799bf46297abe5
Jiří Strnadel <Juzba88@gmail.com>
2025-02-24 01:47:18 +0100

    update

--- src/components/Coordinates.jsx
+++ src/components/Coordinates.jsx
@@ -1,18 +1,34 @@
-import { useState } from "react";
+import "./Coordinate.scss"
+import { useEffect, useState } from 'react';
 
 const Coordinates = () => {
-	const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState({ latitude: '', longitude: '' });
 
-   const api = () =>{
+    console.log(data);
 
-   }
+    useEffect(() => {
+        const url = 'http://api.open-notify.org/iss-now.json ';
+        fetch(url)
+            .then((i) => i.json())
+            .then((data) => {
+                setData({ latitude: data['iss_position']['latitude'], longitude: data['iss_position']['longitude'] });
+                setLoading(false);
+            });
+    }, []);
 
-	return (
-      <div>
-         {loading ? <div>Loading</div> : ""}
-      
-      </div>
-   )
+    return (
+        <div className="coordinates">
+            {loading ? (
+                <div className="loading"><div>loading...</div></div>
+            ) : (
+                <div>
+                    <p>{`Latitude: ${data.latitude}`}</p>
+                    <p>{`Longitude: ${data.longitude}`}</p>
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default Coordinates;
